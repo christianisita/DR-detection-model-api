edbef20e7d3051326177fe07efb63a65806b2fb2
Sita Christiani <sitachristiani@gmail.com>
2021-05-05 06:12:36 +0700

    add upload feature and get prediction from uploaded file data

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from tensorflow import keras
 import numpy as np
+import os
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = './assets/data'
 app = Flask(__name__)
 model = keras.models.load_model("./assets/inception_200_100epoch.h5")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/predict', methods=["GET"])
-def predict():
-    img = keras.preprocessing.image.load_img("./assets/data/0f6e645466a2.png", target_size=(224, 224))
+@app.route('/upload', methods=["POST"])
+def upload():
+    file = request.files['']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return jsonify({"Success": "True",
+                    "message": "success upload file",
+                    "data": {
+                        "img_path": str(filename)
+                    }}, 201)
+
+
+@app.route('/predict/<file_name>', methods=["GET"])
+def predict(file_name):
+    img_path = UPLOAD_FOLDER + '/' + file_name
+    img = keras.preprocessing.image.load_img(img_path, target_size=(224, 224))
     x = keras.preprocessing.image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     images = np.vstack([x])
@@ -17,7 +34,12 @@ def predict():
         predicted = 1
     else:
         predicted = 0
-    return jsonify({"prediction": str(predicted)})
+    result = predicted
+    return jsonify({"success": "True",
+                    "message": "Success getting prediction",
+                    "data": {
+                        "prediction": str(result)
+                    }}, 200)
 
 
 if __name__ == '__main__':
